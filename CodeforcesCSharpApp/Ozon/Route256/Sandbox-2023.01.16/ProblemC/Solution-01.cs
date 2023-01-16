c6fbc63d7ed5ec02a2144c91fd79611c7a638c2c
Vladimir Dmitriev <dmitriev@ofd.ru>
2023-01-16 18:56:00 +0300

    Add Problem C of OZON Route 256 sandbox (2023.01.16)

---
+++ CodeforcesCSharpApp/Ozon/Route256/Sandbox-2023.01.16/ProblemC/Solution-01.cs
@@ -0,0 +1,23 @@
+namespace CodeforcesCSharpApp.Ozon.Route256.Sandbox_20230116.ProblemC01;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var t = int.Parse(Console.ReadLine()!);
+        
+        for (var i = 0; i < t; i++)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+            var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var skills = new Dictionary<int, int>();
+            var positions = new List<int>();
+            
+            
+
+
+            // Console.WriteLine(totalPrice);
+        }
+    }
+}
