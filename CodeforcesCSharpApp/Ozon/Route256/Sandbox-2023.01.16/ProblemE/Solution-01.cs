0843c62756bf93d27609f52cc9875c284f9695b6
Vladimir Dmitriev <dmitriev@ofd.ru>
2023-04-17 19:51:17 +0300

    Using HashSet instead List in Solution #01 of Problem E

--- CodeforcesCSharpApp/Ozon/Route256/Sandbox-2023.01.16/ProblemE/Solution-01.cs
+++ CodeforcesCSharpApp/Ozon/Route256/Sandbox-2023.01.16/ProblemE/Solution-01.cs
@@ -5,27 +5,24 @@ public static class Program
     public static void Main(string[] args)
     {
         var t = int.Parse(Console.ReadLine()!);
-        
+
         for (var i = 0; i < t; i++)
         {
             var n = int.Parse(Console.ReadLine()!);
             var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
 
             var previousTaskId = 0;
-            var tasks = new List<int>();
-
+            var tasks = new HashSet<int>();
             var flag = false;
 
             for (var j = 0; j < n; j++)
             {
                 var taskId = a[j];
-                
+
                 if (previousTaskId == 0)
                     previousTaskId = taskId;
 
-                var exist = tasks.Contains(taskId);
-
-                if (!exist)
+                if (!tasks.Contains(taskId))
                 {
                     tasks.Add(taskId);
                     previousTaskId = taskId;
@@ -33,14 +30,13 @@ public static class Program
                 else if (previousTaskId != taskId)
                 {
                     flag = true;
-                    
                     Console.WriteLine("NO");
-                    
+
                     break;
                 }
             }
 
-            if(!flag)
+            if (!flag)
                 Console.WriteLine("YES");
         }
     }
